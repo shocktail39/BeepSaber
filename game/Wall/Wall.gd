556b20ada7bcc04774aaccc8cb5f75dbc74b8f47
steve hocktail <mvboy39@gmail.com>
2024-10-02 22:52:53 -0400

    minor cleanup to math that positions walls

--- game/Wall/Wall.gd
+++ game/Wall/Wall.gd
@@ -17,21 +17,21 @@ func spawn(wall_info: ObstacleInfo, current_beat: float) -> void:
 	var m := mesh.mesh as BoxMesh
 	var shape := ($WallMeshOrientation/WallArea/CollisionShape3D as CollisionShape3D).shape as BoxShape3D
 	
-	var x := wall_info.width * Constants.LANE_DISTANCE
-	var y := wall_info.height * Constants.LANE_DISTANCE
-	var z := wall_info.duration * Constants.BEAT_DISTANCE
-	var depth := z * 0.5
-	m.size.x = x
-	shape.size.x = x
-	m.size.y = y
-	shape.size.y = y
-	m.size.z = z
-	shape.size.z = z
+	var x_size := wall_info.width * Constants.LANE_DISTANCE
+	var y_size := wall_info.height * Constants.LANE_DISTANCE
+	var z_size := wall_info.duration * Constants.BEAT_DISTANCE
+	var depth := z_size * 0.5
+	m.size.x = x_size
+	shape.size.x = x_size
+	m.size.y = y_size
+	shape.size.y = y_size
+	m.size.z = z_size
+	shape.size.z = z_size
 	despawn_z = Constants.MISS_Z + depth
-	(mesh.material_override as ShaderMaterial).set_shader_parameter(&"size", Vector3(x, y, z))
+	(mesh.material_override as ShaderMaterial).set_shader_parameter(&"size", Vector3(x_size, y_size, z_size))
 	
-	transform.origin.x = (wall_info.line_index - ((4 - wall_info.width) * 0.5)) * Constants.LANE_DISTANCE
-	transform.origin.y = (wall_info.line_layer + (wall_info.height * 0.5)) *  Constants.LANE_DISTANCE
+	transform.origin.x = (0.5 * wall_info.width + wall_info.line_index - 2) * Constants.LANE_DISTANCE
+	transform.origin.y = (0.5 * wall_info.height + wall_info.line_layer) * Constants.LANE_DISTANCE
 	transform.origin.z = (current_beat - wall_info.beat) * Constants.BEAT_DISTANCE - depth
 	
 	speed = Constants.BEAT_DISTANCE * Map.current_info.beats_per_minute / 60.0
