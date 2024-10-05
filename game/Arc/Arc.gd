6673c3df1e950709eb2cfff414f81b4df9fa3c01
steve hocktail <mvboy39@gmail.com>
2024-10-04 20:43:24 -0400

    made arcs mostly functional.  still missing magnetize.

--- game/Arc/Arc.gd
+++ game/Arc/Arc.gd
@@ -4,12 +4,12 @@ class_name Arc
 static var left_material := load("res://game/Arc/Arc.material").duplicate() as ShaderMaterial
 static var right_material := left_material.duplicate() as ShaderMaterial
 
-var color: int
 var speed: float
+var despawn_z: float
 
 func spawn(info: ArcInfo, current_beat: float) -> void:
 	speed = Constants.BEAT_DISTANCE * Map.current_info.beats_per_minute * 0.016666666666666667
-	color = info.color
+	($Visual as CSGPolygon3D).material_override = right_material if info.color == 1 else left_material
 	
 	var head_pos := Vector3(
 		Constants.LANE_DISTANCE * float(info.head_line_index) + Constants.LANE_ZERO_X,
@@ -21,15 +21,26 @@ func spawn(info: ArcInfo, current_beat: float) -> void:
 		Constants.LANE_DISTANCE * float(info.tail_line_layer) + Constants.LAYER_ZERO_Y,
 		-(info.tail_beat - current_beat) * Constants.BEAT_DISTANCE
 	)
-	var mid_pos := head_pos.lerp(tail_pos, 0.5)
+	despawn_z = Constants.MISS_Z - tail_pos.z
 	
-	var path := $Path3D as Path3D
-	print(head_pos)
-	path.curve.set_point_position(0, head_pos)
-	path.curve.set_point_position(1, mid_pos)
-	path.curve.set_point_position(2, tail_pos)
+	var head_rotation: Vector2
+	var tail_rotation: Vector2
+	if info.head_cut_direction == 8:
+		head_rotation = Vector2.ZERO
+	else:
+		head_rotation = Constants.ROTATION_UNIT_VECTORS[info.head_cut_direction] * info.head_control_point_length_multiplier
+	if info.tail_cut_direction == 8:
+		tail_rotation = Vector2.ZERO
+	else:
+		tail_rotation = -Constants.ROTATION_UNIT_VECTORS[info.tail_cut_direction] * info.tail_control_point_length_multiplier
+	var curve := ($Path3D as Path3D).curve
+	curve.set_point_position(0, head_pos)
+	curve.set_point_position(1, tail_pos)
+	curve.set_point_out(0, Vector3(head_rotation.x, head_rotation.y, 0.0))
+	curve.set_point_in(1, Vector3(tail_rotation.x, tail_rotation.y, 0.0))
 
 func _process(delta: float) -> void:
+	if Scoreboard.paused or not is_visible_in_tree() or not Map.current_info: return
 	transform.origin.z += speed * delta
-	if transform.origin.z > 16.0:
+	if transform.origin.z >= despawn_z:
 		queue_free()
--- game/Constants.gd
+++ game/Constants.gd
@@ -6,4 +6,9 @@ const BEAT_DISTANCE := 4.0
 const LANE_DISTANCE := 0.6
 const MISS_Z := 2.5
 var CUBE_ROTATIONS := PackedFloat64Array([PI, 0.0, -TAU*0.25, TAU*0.25, -TAU*0.375, TAU*0.375, -TAU*0.125, TAU*0.125, 0.0])
+var ROTATION_UNIT_VECTORS := PackedVector2Array([
+	Vector2(0, 1), Vector2(0, -1), Vector2(-1, 0), Vector2(1, 0),
+	Vector2(-0.70710678, 0.70710678), Vector2(0.70710678, 0.70710678),
+	Vector2(-0.70710678, -0.70710678), Vector2(0.70710678, -0.70710678), Vector2(0,1)
+])
 const APPDATA_PATH := "user://OpenSaber/"
